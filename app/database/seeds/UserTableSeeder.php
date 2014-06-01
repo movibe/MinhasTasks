4c752f5ccee17f541ad2063023566cd0337ff163
Willian Ribeiro Angelo <agfoccus@gmail.com>
2014-06-01 13:02:26 -0300

    User Seeder com usuario teste@teste.com

--- app/database/seeds/UserTableSeeder.php
+++ app/database/seeds/UserTableSeeder.php
@@ -4,8 +4,16 @@ class UserTableSeeder extends Seeder {
 	
 	public function run()
 	{
-// Uncomment the below to wipe the table clean before populating
-// DB::table('user')->truncate();
+	// Uncomment the below to wipe the table clean before populating
+	// DB::table('user')->truncate();
+		
+		User::create([
+		             'nome'     => 'Usuário',
+		             'email'    => 'teste@teste.com',
+		             'password' => Hash::make('teste'),
+		             'active'   => true
+		             ]);
+		
 		
 		User::create([
 		             'nome'     => 'Willian',
@@ -14,16 +22,8 @@ class UserTableSeeder extends Seeder {
 		             'active'   => true
 		             ]);
 		
-		
-		User::create([
-		             'nome'     => 'Natalia',
-		             'email'    => 'natalia.araujo.rodrigues@gmail.com',
-		             'password' => Hash::make('demo'),
-		             'active'   => true
-		             ]);
-		
-// Uncomment the below to run the seeder
-// DB::table('user')->insert($user);
+	// Uncomment the below to run the seeder
+	// DB::table('user')->insert($user);
 	}
 	
 }
