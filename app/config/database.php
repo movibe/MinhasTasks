03fee2fcb9434266badaeee0d67ecc0af48abb8b
Willian Ribeiro Angelo <agfoccus@gmail.com>
2014-06-01 22:27:38 -0300

    Database

--- app/config/database.php
+++ app/config/database.php
@@ -53,15 +53,15 @@ return array(
 		),
 
 		'mysql' => array(
-			'driver'    => 'mysql',
-			'host'      => $_SERVER['DB1_HOST'],
-			'database'  => $_SERVER['DB1_NAME'],
-			'username'  => $_SERVER['DB1_USER'],
-			'password'  => $_SERVER['DB1_PASS'],
-			'charset'   => 'utf8',
-			'collation' => 'utf8_unicode_ci',
-			'prefix'    => '',
-		),
+		'driver'    => 'mysql',
+		'host'      => $_SERVER['DB1_HOST'],
+		'database'  => $_SERVER['DB1_NAME'],
+		'username'  => $_SERVER['DB1_USER'],
+		'password'  => $_SERVER['DB1_PASS'],
+		'charset'   => 'utf8',
+		'collation' => 'utf8_unicode_ci',
+		'prefix'    => '',
+	),
 
 		'pgsql' => array(
 			'driver'   => 'pgsql',
