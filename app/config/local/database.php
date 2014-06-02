6793b9dffe83f0e95cdcc06fa985a45a443e7286
Willian Ribeiro Angelo <agfoccus@gmail.com>
2014-06-01 23:37:44 -0300

    Configuração Local

---
+++ app/config/local/database.php
@@ -0,0 +1,22 @@
+<?php
+
+return array(
+
+	
+	'connections' => array(
+
+
+		'mysql' => array(
+		'driver'    => 'mysql',
+		'host'      => 'localhost',
+		'database'  => 'lv_tarefas',
+		'username'  => 'root',
+		'password'  => 'mysql',
+		'charset'   => 'utf8',
+		'collation' => 'utf8_unicode_ci',
+		'prefix'    => '',
+	),
+
+	),
+
+);
