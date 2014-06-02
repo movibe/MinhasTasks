638948555828182d6dba836dfa1fda3f4762e9dd
Willian Ribeiro Angelo <agfoccus@gmail.com>
2014-06-01 21:56:33 -0300

    Database Pagodabox

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -27,7 +27,7 @@ $app = new Illuminate\Foundation\Application;
 $env = $app->detectEnvironment(array(
 
 	'local' => array('localhost'),
-	'pagodabox' => array('minhastasks.*')
+	'pagodabox' => array('*.gopagoda.com')
 
 ));
 
